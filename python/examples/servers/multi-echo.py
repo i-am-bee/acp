047679c559c87951305db1d1147b4fec9f1a3214
Tomas Pilar <tomas.pilar@apoco.com>
2025-04-14 09:28:14 +0200

    fixup!
    Signed-off-by: Tomas Pilar <tomas.pilar@apoco.com>

--- python/examples/servers/multi-echo.py
+++ python/examples/servers/multi-echo.py
@@ -3,7 +3,7 @@ from collections.abc import AsyncGenerator, Generator
 from acp_sdk.models import (
     Message,
 )
-from acp_sdk.server import Context, RunYield, RunYieldResume, Server
+from acp_sdk.server import Agent, Context, RunYield, RunYieldResume, Server
 
 # This example showcases several ways to create echo agent using decoratos.
 
@@ -36,6 +36,24 @@ def sync_echo(input: Message, context: Context) -> RunYield:
     """Echoes everything"""
     context.yield_sync({"thought": "I should echo everyting"})
     return input
+
+
+class EchoAgent(Agent):
+    @property
+    def name(self) -> str:
+        return "class_echo"
+
+    @property
+    def description(self) -> str:
+        return "Echoes everything"
+
+    async def run(self, input: Message, context: Context) -> AsyncGenerator[RunYield, RunYieldResume]:
+        """Echoes everything"""
+        yield {"thought": "I should echo everyting"}
+        yield input
+
+
+server.register(EchoAgent())
 
 
 server.run()
