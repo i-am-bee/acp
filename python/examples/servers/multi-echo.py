2831aabad4413a88df1da0462ff1b36ffd71a1bc
Tomas Pilar <tomas.pilar@apoco.com>
2025-04-14 09:12:39 +0200

    fixup!
    Signed-off-by: Tomas Pilar <tomas.pilar@apoco.com>

--- python/examples/servers/multi-echo.py
+++ python/examples/servers/multi-echo.py
@@ -10,28 +10,28 @@ from acp_sdk.server import Context, RunYield, RunYieldResume, Server
 server = Server()
 
 
-@server.agent(description="Async generator")
+@server.agent()
 async def async_gen_echo(input: Message, context: Context) -> AsyncGenerator[RunYield, RunYieldResume]:
     """Echoes everything"""
     yield {"thought": "I should echo everyting"}
     yield input
 
 
-@server.agent(description="Async")
+@server.agent()
 async def async_echo(input: Message, context: Context) -> RunYield:
     """Echoes everything"""
     await context.yield_async({"thought": "I should echo everyting"})
     return input
 
 
-@server.agent(description="Generator")
+@server.agent()
 def gen_echo(input: Message, context: Context) -> Generator[RunYield, RunYieldResume]:
     """Echoes everything"""
     yield {"thought": "I should echo everyting"}
     return input
 
 
-@server.agent(description="Sync")
+@server.agent()
 def sync_echo(input: Message, context: Context) -> RunYield:
     """Echoes everything"""
     context.yield_sync({"thought": "I should echo everyting"})
