7e189912f1a803826a2f768c28ef03848912ff4a
jenna-winkler <jw.8378@gmail.com>
2025-04-22 11:29:06 -0400

    docs: examples
    Signed-off-by: jenna-winkler <jw.8378@gmail.com>

---
+++ examples/integrations/langgraph/client_stream.py
@@ -0,0 +1,26 @@
+import asyncio
+
+from acp_sdk.client import Client
+from acp_sdk.models import (
+    Message,
+    MessagePart,
+    MessageCompletedEvent,
+    GenericEvent
+)
+
+
+async def client() -> None:
+    async with Client(base_url="http://localhost:8000") as client:
+        async for event in client.run_stream(
+            agent="lang_grapt_agent", 
+            inputs=[Message(parts=[MessagePart(content="Lukas", content_type="text/plain")])]
+        ):
+            match event:
+                case MessageCompletedEvent():
+                    print("\nFinal response:", event.message)
+                case GenericEvent():
+                    print(event.generic.update)
+
+
+if __name__ == "__main__":
+    asyncio.run(client())
