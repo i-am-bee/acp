3acebaa23446bd6ec2839174ebc03095744d7705
Tomas Weiss <tomas.weiss2@gmail.com>
2025-04-22 14:43:46 +0200

    fix: code review improvements
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>

--- examples/python/prompt-chaining/agent.py
+++ examples/python/prompt-chaining/agent.py
@@ -20,36 +20,31 @@ async def run_agent(agent: str, input: str) -> list[Message]:
 
     return run.outputs
 
-def get_text_from_messages(messages: list[Message]) -> str:
-    if not messages or not messages[0].parts:
-        return ""
-    return messages[0].parts[0].content
-
 @server.agent(name="translation")
 async def translation_agent(inputs: list[Message]) -> AsyncGenerator:
     llm = ChatModel.from_name("ollama:llama3.1:8b")
 
     agent = ReActAgent(llm=llm, tools=[], memory=TokenMemory(llm))
-    response = await agent.run(prompt="Translate the given text to Spanish. The text is: " + get_text_from_messages(inputs))
+    response = await agent.run(prompt="Translate the given text to Spanish. The text is: " + str(inputs))
 
-    yield Message(parts=[MessagePart(content=response.result.text)])
+    yield MessagePart(content=response.result.text)
 
 @server.agent(name="marketing_copy") 
 async def marketing_copy_agent(inputs: list[Message]) -> AsyncGenerator:
     llm = ChatModel.from_name("ollama:llama3.1:8b")
 
     agent = ReActAgent(llm=llm, tools=[], memory=TokenMemory(llm))
-    response = await agent.run(prompt="You are able to generate punchy headlines for a marketing campaign. Provide punchy headline to sell the specified product on users eshop. The product is: " + get_text_from_messages(inputs))
+    response = await agent.run(prompt="You are able to generate punchy headlines for a marketing campaign. Provide punchy headline to sell the specified product on users eshop. The product is: " + str(inputs))
 
-    yield Message(parts=[MessagePart(content=response.result.text)])
+    yield MessagePart(content=response.result.text)
 
 
 @server.agent(name="assistant")
 async def main_agent(inputs: list[Message], context: Context) -> AsyncGenerator:
-    marketing_copy = await run_agent("marketing_copy", get_text_from_messages(inputs))
-    translated_marketing_copy = await run_agent("translation", get_text_from_messages(marketing_copy))
+    marketing_copy = await run_agent("marketing_copy", str(inputs))
+    translated_marketing_copy = await run_agent("translation", str(marketing_copy))
 
-    yield Message(parts=[MessagePart(content=get_text_from_messages(marketing_copy)), MessagePart(content=get_text_from_messages(translated_marketing_copy))])
+    yield MessagePart(content=str(marketing_copy[0]))
+    yield MessagePart(content=str(translated_marketing_copy[0]))
 
-if __name__ == "__main__":
-    server.run()
+server.run()
